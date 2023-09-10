73736f8428a12af959cfac136cc5ae18d3e91af3
Souradeep <jeetsouradeep@gmail.com>
2023-09-10 12:04:59 -0400

    Daily leetcode

---
+++ DP/Dp_on_sequ/Target_sum_signs.py
@@ -0,0 +1,43 @@
+def countPartitionsUtil(ind, target, arr, dp):
+    if ind == 0:
+        if target == 0 and arr[0] == 0:
+            return 2
+        if target == 0 or target == arr[0]:
+            return 1
+        return 0
+
+    if dp[ind][target] != -1:
+        return dp[ind][target]
+
+    notTaken = countPartitionsUtil(ind-1, target, arr, dp)
+    taken = 0
+    if arr[ind] <= target:
+        taken = countPartitionsUtil(ind-1, target-arr[ind], arr, dp)
+
+    dp[ind][target] = notTaken + taken
+    return dp[ind][target]
+
+def targetSum(n, target, arr):
+    totSum = 0
+    for i in range(len(arr)):
+        totSum += arr[i]
+
+    # Checking for edge cases
+    if totSum-target < 0:
+        return 0
+    if (totSum-target) % 2 == 1:
+        return 0
+
+    s2 = (totSum-target) // 2
+
+    dp = [[-1 for j in range(s2 + 1)] for i in range(n)]
+    return countPartitionsUtil(n-1, s2, arr, dp)
+
+def main():
+    arr = [1, -2, 3, 1]
+    target = 3
+    n = len(arr)
+    print("The number of ways found is", targetSum(n, target, arr))
+
+if __name__ == '__main__':
+    main()
