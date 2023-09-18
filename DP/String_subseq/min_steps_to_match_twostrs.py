87bae4060ad489c8b9ba90bbbe7a5ceba0ba4495
Souradeep <jeetsouradeep@gmail.com>
2023-09-18 16:18:12 -0400

    Daily leetcode

---
+++ DP/String_subseq/min_steps_to_match_twostrs.py
@@ -0,0 +1,27 @@
+def lcsUtil(s1, s2, ind1, ind2, dp):
+    if ind1 < 0:
+        return ind2 + 1
+    if ind2 < 0:
+        return ind1 + 1
+    if dp[ind1][ind2] != -1:
+        return dp[ind1][ind2]
+    if s1[ind1] == s2[ind2]:
+        dp[ind1][ind2] = 0+ lcsUtil(s1, s2, ind1 - 1, ind2 - 1, dp)
+    else:
+        dp[ind1][ind2] = 1+ min(lcsUtil(s1, s2, ind1 - 1, ind2 - 1, dp), min(lcsUtil(s1, s2, ind1 - 1, ind2, dp), lcsUtil(s1, s2, ind1, ind2-1, dp)))
+    return dp[ind1][ind2]
+
+def lcs(s1, s2):
+    n = len(s1)
+    m = len(s2)
+    dp = [[-1 for j in range(m)] for i in range(n)]
+    k = lcsUtil(s1, s2, n - 1, m - 1, dp)
+    return k
+
+def main():
+    s1 = "horse"
+    s2 ="ros"
+    print("The minimum number of operations required is", lcs(s1, s2))
+
+if __name__ == '__main__':
+    main()
